4e47002005a42800389eea3467bf1989cbe3b2c9
PaulModaley <pmodaley@gmail.com>
2022-07-08 14:18:06 +0000

    register models in admin.py

--- restaurant/admin.py
+++ restaurant/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Booking, UserProfile, Contact
 
 # Register your models here.
+admin.site.site_header = "Pauls Kitchen Dashboard"
+
+
+class BookingAdmin(admin.ModelAdmin):
+    profile = "/workspace/paulskitchen/templates/restaurant/profile.html"
+
+
+admin.site.register(Booking)
+admin.site.register(UserProfile)
+admin.site.register(Contact)
