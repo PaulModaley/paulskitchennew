3251ed7e93140ce138cb4579a1f2fd08ca2ff31d
PaulModaley <pmodaley@gmail.com>
2022-07-08 14:17:16 +0000

    continue writing models for booking and user profile and write contact model

--- restaurant/models.py
+++ restaurant/models.py
@@ -1,14 +1,53 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+time_choices = (
+    ("11:00", "11:00"),
+    ("12:00", "12:00"),
+    ("13:00", "13:00"),
+    ("14:00", "14:00"),
+    ("15:00", "15:00"),
+    ("16:00", "16:00"),
+    ("17:00", "17:00"),
+    ("18:00", "18:00"),
+    ("19:00", "19:00"),
+    ("20:00", "20:00"),
+    ("21:00", "21:00"),
+    ("22:00", "22:00"),
+)
+
+guest_choices = (
+    (1, "1 person"),
+    (2, "2 people"),
+    (3, "3 people"),
+    (4, "4 people"),
+    (5, "5 people"),
+    (6, "6 people"),
+    (7, "7 people"),
+    (8, "8 people"),
+)
+
 # Create your models here.
 class Booking(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
     date = models.DateField(null=True)
-    time = models.TimeField(null=True)
-    guests = models.IntegerField(null=True)
+    time = models.CharField(
+        max_length=10, choices=time_choices, default="12:00", null=True
+    )
+    guests = models.IntegerField(choices=guest_choices, default=1)
+
 
 class UserProfile(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     firstname = models.CharField(max_length=100, null=True)
     lastname = models.CharField(max_length=100, null=True)
+    email = models.CharField(max_length=100, null=True)
+
+
+"""model for form"""
+
+
+class Contact(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField(max_length=250)
+    message = models.CharField(max_length=500)
